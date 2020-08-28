8bb01bcc3ead4453c91008df4052d6326633c439
kristofferberg-vipps <kristoffer.hovland.berg@vipps.no>
2020-08-28 17:41:31 +0200

    Integrate postgres interaction

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/kristohberg/graphkul
 go 1.13
 
 require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.12.2
 	github.com/graphql-go/graphql v0.7.9
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/lib/pq v1.8.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 
 	"github.com/kristohberg/graphkul/handlers"
 	"github.com/kristohberg/graphkul/models"
@@ -10,19 +16,48 @@ import (
 
 // GraphqlAPI is a global graphql struct
 type GraphqlAPI struct {
-	Api handlers.Api
+	API handlers.Api
 }
 
 // Test graphql for go
 
 func main() {
+
+	db, err := sql.Open("postgres", "postgres://localhost:5432/database?sslmode=enable")
+	if err != nil {
+		log.Fatal("not able to connect to postgres db")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("not able to ping database:  ")
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal("not able to get driver")
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://db/migrations", "postgres", driver)
+	if err != nil {
+		log.Fatal("not able to get migrate instance")
+	}
+	err = m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		err = m.Down()
+		if err != nil {
+			log.Fatal("not able to migrate down")
+		}
+		log.Fatal("not able to migrate")
+	}
+
 	e := echo.New()
 	newGraphqlSchema, err := models.NewSchema()
 	if err != nil {
 		log.Fatal("not able to generate graphql schema")
 	}
-	newApp := GraphqlAPI{Api: handlers.Api{Schema: newGraphqlSchema}}
-	newApp.Api.Handler(e)
+	newApp := GraphqlAPI{API: handlers.Api{Schema: newGraphqlSchema}}
+	newApp.API.Handler(e)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
